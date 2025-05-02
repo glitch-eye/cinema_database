dc00d34a87ac8d68146751cc650762c6e3f71a70
pdung313 <dung.phambuitri2005@hcmut.edu.vn>
2025-05-02 09:22:57 +0700

    function_total_revenue.sql

--- function_total_revenue.sql
+++ function_total_revenue.sql
@@ -1,4 +1,4 @@
-CREATE FUNCTION GetTotalRevenue (@InputDate DATE)
+CREATE FUNCTION GetTotalRevenue (@InputDate DATE, @CinemaID INT = NULL)
 RETURNS DECIMAL(18, 2)
 AS
 BEGIN
@@ -6,17 +6,23 @@ BEGIN
 
     SELECT @TotalRevenue = ISNULL(SUM(Total), 0)
     FROM (
+        -- Revenue from tickets
         SELECT SUM(t.Cost) AS Total
         FROM TICKET t
         JOIN Receipt r ON t.Trans_ID = r.Receipt_ID
+        JOIN Theatre th ON t.Room_ID = th.ID
         WHERE CAST(r.Receipt_Date AS DATE) = @InputDate
+          AND (@CinemaID IS NULL OR th.C_ID = @CinemaID)
 
         UNION ALL
 
+        -- Revenue from services
         SELECT SUM(sc.Amount) AS Total
         FROM Service_cost sc
         JOIN Receipt r ON sc.Tran_ID = r.Receipt_ID
+        JOIN Services_Theatre st ON sc.Ser_Bou_ID = st.Ser_ID
         WHERE CAST(r.Receipt_Date AS DATE) = @InputDate
+          AND (@CinemaID IS NULL OR st.T_ID = @CinemaID)
     ) AS Revenue;
 
     RETURN @TotalRevenue;
