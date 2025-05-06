35c82001de3fbeed674047b855f56e82f3f3d11e
Plebblan <62107332+Plebblan@users.noreply.github.com>
2025-05-06 07:29:13 +0700

    done
    I slep now

---
+++ data_layer_4.sql
@@ -0,0 +1,16 @@
+INSERT INTO Ticket_Discount (Event_Name, Tic_ID, T_Type, T_Value) VALUES
+('Student Night Discount', 1, 'T', 2), 
+('Student Night Discount', 2, 'T', 2), 
+('Student Night Discount', 3, 'T', 2), 
+('Student Night Discount', 4, 'T', 2), 
+('Student Night Discount', 5, 'T', 2), 
+('Student Night Discount', 6, 'T', 4), 
+('Student Night Discount', 7, 'T', 2), 
+('Student Night Discount', 8, 'T', 6), 
+('Student Night Discount', 9, 'T', 2), 
+('Student Night Discount', 10, 'T', 2), 
+('Student Night Discount', 11, 'T', 2), 
+('Student Night Discount', 12, 'T', 2), 
+('Student Night Discount', 13, 'T', 4), 
+('Student Night Discount', 14, 'T', 2), 
+('Student Night Discount', 15, 'T', 2); 
